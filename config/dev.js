0eadb2bcd71d9bbc2fd1e2afde16562742781a6a
kang-heeeun <asd8186@naver.com>
2021-02-28 02:39:13 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://heeeun:abcd1234@boilerplate.sspkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -2,22 +2,39 @@
 //간단하게, 몽고디비를 편하게 쓸수 있는 object modeling tool
 const express = require("express"); //express 갖고오기
 const app = express(); // 새로운 express 앱 만들기
-const port = 3000;
+const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true })); //application/x-ww-form-urlencoded
+app.use(bodyParser.json()); //application/json
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://heeeun:abcd1234@boilerplate.sspkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello World!~~ 안녕하세요 ~"));
 
-app.listen(port, () => console.log(`Expddd ${port}`));
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listenling on port ${port}!`));
